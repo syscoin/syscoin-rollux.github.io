5d61b613642db1c67e81e12c1292ede509c889a2
Fernando Paredes Garcia <fernando@develcuy.com>
2023-06-26 19:33:27 -0500

    Update chain names and restore tesnet contracts data.

--- src/chains.ts
+++ src/chains.ts
@@ -1,9 +1,9 @@
 import { ethers } from 'ethers'
 
 export const NETWORK_DATA = {
-  tanenbaum: {
+  "syscoin-tanenbaum": {
     id: 5700,
-    name: 'Tanenbaum',
+    name: 'Syscoin Tanenbaum (testnet)',
     provider: new ethers.providers.StaticJsonRpcProvider(
       'https://rpc.tanenbaum.io'
     ),
@@ -11,29 +11,9 @@ export const NETWORK_DATA = {
     pair: 'optimism',
     bridge: '0x77Cdc3891C91729dc9fdea7000ef78ea331cb34A',
   },
-  rollux: {
-    id: 57000,
-    name: 'RolluxTest',
-    layer: 2,
-    provider: new ethers.providers.StaticJsonRpcProvider(
-      'https://rpc-tanenbaum.rollux.com/'
-    ),
-    pair: 'optimism',
-    bridge: '0x4200000000000000000000000000000000000010',
-  },
-  rollux_mainnet: {
-    id: 570,
-    name: 'RolluxMainnet',
-    layer: 2,
-    provider: new ethers.providers.StaticJsonRpcProvider(
-      'https://rpc.rollux.com'
-    ),
-    pair: 'optimism',
-    bridge: '0x4200000000000000000000000000000000000010',
-  },
-  syscoin_nevm: {
+  syscoin: {
     id: 57,
-    name: 'SyscoinNevm',
+    name: 'Syscoin Mainnet',
     provider: new ethers.providers.StaticJsonRpcProvider(
       'https://rpc.syscoin.org'
     ),
@@ -41,5 +21,24 @@ export const NETWORK_DATA = {
     pair: 'optimism',
     bridge: '0x9cc66f9B7b07F72a487FF751a7cBE281976fce7C',
   },
-  
+  "rollux-tanenbaum": {
+    id: 57000,
+    name: 'Rollux Tanenbaum (testnet)',
+    layer: 2,
+    provider: new ethers.providers.StaticJsonRpcProvider(
+      'https://rpc-tanenbaum.rollux.com/'
+    ),
+    pair: 'optimism',
+    bridge: '0x4200000000000000000000000000000000000010',
+  },
+  rollux: {
+    id: 570,
+    name: 'Rollux Mainnet',
+    layer: 2,
+    provider: new ethers.providers.StaticJsonRpcProvider(
+      'https://rpc.rollux.com'
+    ),
+    pair: 'optimism',
+    bridge: '0x4200000000000000000000000000000000000010',
+  },
 }
